d8b1a982745a4b010335369abef1a3fb6b5edb04
aslanzade700 <aslanzade.700+github@gmail.com>
2026-02-13 10:55:39 +0400

    did the lab exercise 3 (a, b)

---
+++ week03/invoice/Invoice.java
@@ -0,0 +1,46 @@
+package week03.invoice;
+    public class Invoice {
+        String number;
+        String description;
+        int quantity;
+        double price;
+        
+        public Invoice(String number, String description, int quantity, double price) {
+            this.number = number;
+            this.description = description;
+            this.quantity = quantity;
+            this.price = price;
+        }
+        
+        public void setNumber(String number) {
+            this.number = number;
+        }
+    
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    
+        public void setPrice(double price) {
+            this.price = price;
+        }
+    
+        public String getNumber() {
+            return number;
+        }
+    
+        public String getDescription() {
+            return description;
+        }
+    
+        public int getQuantity() {
+            return quantity;
+        }
+        
+        public double getPrice() {
+            return price;
+        }
+    }
